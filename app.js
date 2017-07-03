967b6246fdb0e573d44341ca181ea20bd8afa320
Andrew Robida <afrobida@gmail.com>
2017-07-03 23:29:14 +0000

    added function to compare correct and incorrect guesses and store them in their appropriate objects and either subtract or not from the total amount of guesses

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.controller("GameController",["$scope", function($scope){
 var words=["rat","cat","bat","mat"];
 $scope.incorrectLettersChosen=[];
 $scope.correctLettersChosen=[];
+var selectedWord='';
 $scope.guesses=6;
 $scope.displayWord='';
 $scope.input={
@@ -29,6 +30,38 @@ var newGame=function(){
     }
     console.log(tempDisplayWord);
     $scope.displayWord=tempDisplayWord;
+}
+
+$scope.letterChosen=function(){
+    
+    for (var i = 0;i < $scope.correctLettersChosen.length; i++) {
+        if($scope.correctLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+            $scope.input.letter="";
+            return;
+        }
+    }
+    
+    for (var i = 0;i < $scope.incorrectLettersChosen.length; i++) {
+        if($scope.incorrectLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+            $scope.input.letter="";
+            return;
+        }
+    }
+    
+    var correct=false;
+    for (var i = 0;i < selectedWord.length; i++) {
+        if(selectedWord[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+            $scope.displayWord=$scope.displayWord.slice(0,i)+$scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1)
+            correct=true;
+        }
+    }
+    if(correct){
+        $scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+    }else{
+        $scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase());
+    }
+    $scope.input.letter="";
+    
 }
 
 newGame();    
